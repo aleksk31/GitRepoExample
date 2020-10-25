975eb5bb4ba919f8764218fb1bc60a88cd19038c
Alexey Karenko <aleksk779@gmail.com>
2020-10-25 08:45:54 +0100

    Initial commit for a project

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    // we need to initialize the system control versions.
+    // it is a console version, but can do it from interface as well
+    //
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
