2a7b85d3b6bdfcd14c287b2ab42d34f1eac92d9e
Alexey Karenko <aleksk779@gmail.com>
2020-10-25 08:59:21 +0100

    Initial commit for a project

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,7 @@ public class App
 {
     // we need to initialize the system control versions.
     // it is a console version, but can do it from interface as well
+    // after commit files are fixed but still local
     //
     public static void main( String[] args )
     {
